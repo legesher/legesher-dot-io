6fafd14a9a87aad4acf00aadc46db052e78218c4
madipfaff <pfaffmadison@gmail.com>
2018-09-23 23:30:31 -0400

    :bug: fix redirect to mailchimp to stay on main webpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,137 +1,137 @@
-/*
-	Eventually by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-(function() {
-
-	"use strict";
-
-	var	$body = document.querySelector('body');
-
-	// Methods/polyfills.
-
-		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
-
-		// canUse
-			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
-
-		// window.addEventListener
-			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
-
-
-	// Signup Form.
-		(function() {
-
-			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-					$message;
-
-			// Bail if addEventListener isn't supported.
-				if (!('addEventListener' in $form))
-					return;
-
-			// Message.
-				$message = document.createElement('span');
-					$message.classList.add('message');
-					$form.appendChild($message);
-
-				$message._show = function(type, text) {
-
-					$message.innerHTML = text;
-					$message.classList.add(type);
-					$message.classList.add('visible');
-
-					window.setTimeout(function() {
-						$message._hide();
-					}, 3000);
-
-				};
-
-				$message._hide = function() {
-					$message.classList.remove('visible');
-				};
-
-			// Events.
-			// Note: If you're *not* using AJAX, get rid of this event listener.
-				$form.addEventListener('submit', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					// Hide message.
-						$message._hide();
-
-					// Disable submit.
-						$submit.disabled = true;
-
-					// Process form.
-					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-					// but there's enough here to piece together a working AJAX submission call that does.
-						window.setTimeout(function() {
-
-							// Reset form.
-								$form.reset();
-
-							// Enable submit.
-								$submit.disabled = false;
-
-							// Show message.
-								$message._show('success', 'Thank you!');
-								//$message._show('failure', 'Something went wrong. Please try again.');
-
-						}, 750);
-
-				});
-
-		})();
-
-})();
-
-// MAILCHIMP EMAIL
-
-data = [
-    'email'     => 'johndoe@example.com',
-    'status'    => 'subscribed',
-    'firstname' => 'john',
-    'lastname'  => 'doe'
-];
-
-syncMailchimp(data);
-
-function syncMailchimp(data) {
-    apiKey = 'your api key';
-    listId = 'your list id';
-
-    memberId = md5(strtolower(data['email']));
-    dataCenter = substr(apiKey,strpos(apiKey,'-')+1);
-    url = 'https://' . dataCenter . '.api.mailchimp.com/3.0/lists/' . listId . '/members/' . memberId;
-
-    json = json_encode([
-        'email_address' => data['email'],
-        'status'        => data['status'], // "subscribed","unsubscribed","cleaned","pending"
-        'merge_fields'  => [
-            'FNAME'     => data['firstname'],
-            'LNAME'     => data['lastname']
-        ]
-    ]);
-
-    ch = curl_init(url);
-
-    curl_setopt(ch, CURLOPT_USERPWD, 'user:' . apiKey);
-    curl_setopt(ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-    curl_setopt(ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt(ch, CURLOPT_TIMEOUT, 10);
-    curl_setopt(ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-    curl_setopt(ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt(ch, CURLOPT_POSTFIELDS, json);
-
-    result = curl_exec(ch);
-    httpCode = curl_getinfo(ch, CURLINFO_HTTP_CODE);
-    curl_close(ch);
-
-    return httpCode;
-}
+// /*
+// 	Eventually by HTML5 UP
+// 	html5up.net | @ajlkn
+// 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+// */
+//
+// (function() {
+//
+// 	"use strict";
+//
+// 	var	$body = document.querySelector('body');
+//
+// 	// Methods/polyfills.
+//
+// 		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
+// 			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+//
+// 		// canUse
+// 			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
+//
+// 		// window.addEventListener
+// 			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
+//
+//
+// 	// Signup Form.
+// 		(function() {
+//
+// 			// Vars.
+// 				var $form = document.querySelectorAll('#signup-form')[0],
+// 					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+// 					$message;
+//
+// 			// Bail if addEventListener isn't supported.
+// 				if (!('addEventListener' in $form))
+// 					return;
+//
+// 			// Message.
+// 				$message = document.createElement('span');
+// 					$message.classList.add('message');
+// 					$form.appendChild($message);
+//
+// 				$message._show = function(type, text) {
+//
+// 					$message.innerHTML = text;
+// 					$message.classList.add(type);
+// 					$message.classList.add('visible');
+//
+// 					window.setTimeout(function() {
+// 						$message._hide();
+// 					}, 3000);
+//
+// 				};
+//
+// 				$message._hide = function() {
+// 					$message.classList.remove('visible');
+// 				};
+//
+// 			// Events.
+// 			// Note: If you're *not* using AJAX, get rid of this event listener.
+// 				$form.addEventListener('submit', function(event) {
+//
+// 					event.stopPropagation();
+// 					event.preventDefault();
+//
+// 					// Hide message.
+// 						$message._hide();
+//
+// 					// Disable submit.
+// 						$submit.disabled = true;
+//
+// 					// Process form.
+// 					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+// 					// but there's enough here to piece together a working AJAX submission call that does.
+// 						window.setTimeout(function() {
+//
+// 							// Reset form.
+// 								$form.reset();
+//
+// 							// Enable submit.
+// 								$submit.disabled = false;
+//
+// 							// Show message.
+// 								$message._show('success', 'Thank you!');
+// 								//$message._show('failure', 'Something went wrong. Please try again.');
+//
+// 						}, 750);
+//
+// 				});
+//
+// 		})();
+//
+// })();
+//
+// // MAILCHIMP EMAIL
+//
+// data = [
+//     'email'     => 'johndoe@example.com',
+//     'status'    => 'subscribed',
+//     'firstname' => 'john',
+//     'lastname'  => 'doe'
+// ];
+//
+// syncMailchimp(data);
+//
+// function syncMailchimp(data) {
+//     apiKey = 'your api key';
+//     listId = 'your list id';
+//
+//     memberId = md5(strtolower(data['email']));
+//     dataCenter = substr(apiKey,strpos(apiKey,'-')+1);
+//     url = 'https://' . dataCenter . '.api.mailchimp.com/3.0/lists/' . listId . '/members/' . memberId;
+//
+//     json = json_encode([
+//         'email_address' => data['email'],
+//         'status'        => data['status'], // "subscribed","unsubscribed","cleaned","pending"
+//         'merge_fields'  => [
+//             'FNAME'     => data['firstname'],
+//             'LNAME'     => data['lastname']
+//         ]
+//     ]);
+//
+//     ch = curl_init(url);
+//
+//     curl_setopt(ch, CURLOPT_USERPWD, 'user:' . apiKey);
+//     curl_setopt(ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+//     curl_setopt(ch, CURLOPT_RETURNTRANSFER, true);
+//     curl_setopt(ch, CURLOPT_TIMEOUT, 10);
+//     curl_setopt(ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+//     curl_setopt(ch, CURLOPT_SSL_VERIFYPEER, false);
+//     curl_setopt(ch, CURLOPT_POSTFIELDS, json);
+//
+//     result = curl_exec(ch);
+//     httpCode = curl_getinfo(ch, CURLINFO_HTTP_CODE);
+//     curl_close(ch);
+//
+//     return httpCode;
+// }
