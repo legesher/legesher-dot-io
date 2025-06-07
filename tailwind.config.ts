7f0ba66deaae1e66ee34b2575e8cdb1be8446e07
Madison (Pfaff) Edgar <7844510+madiedgar@users.noreply.github.com>
2025-06-07 10:47:43 -0700

    feat: update text font for code editor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
+        'noto-mono': ['Noto Sans Mono', 'monospace'],
       },
       colors: {
         border: 'hsl(var(--border))',
