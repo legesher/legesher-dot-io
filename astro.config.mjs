ab58879b7de2617836bd4548fe15baeffc8a4f0e
Madison (Pfaff) Edgar <7844510+madiedgar@users.noreply.github.com>
2025-06-26 20:05:52 -0700

    fix: update toast and deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [
--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-import { useToast } from "@/hooks/use-toast"
+import { useToast } from "./use-toast"
 import {
   Toast,
   ToastClose,
